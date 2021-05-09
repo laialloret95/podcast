da724702747ea171044a6dfdcbc3bd9ddaacdd8e
Laia Lloret <laialloret95@gmail.com>
2021-05-09 16:16:41 +0200

    get unique genres & 6 newest podcasts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const Podcast = require('../models/podcast');
 const router = express.Router();
 
+
 // HOME PAGE
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Podcast App 🎧' });
+router.get('/', (req, res, next) => {
+    // get unique genres
+    Podcast
+        .find()
+        .distinct("genre")
+        .then(genresDB => {
+            return genresDB;
+        })
+        .then((genresDB) => {
+            // get the 6 newest Podcasts
+            Podcast
+            .find({})
+            .limit(6)
+            .sort({pub_date: -1})
+            .then(podcastsDB => {
+                res.render('index', {podcastsDB, genresDB});
+            });
+        })
+        .catch(error => next(error));
 });
 
 module.exports = router;
