4bdba8abe6698bde631fdd7217dca7a5e9479601
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-05-10 15:11:21 +0200

    users are added succesfully to the DB through the signup form

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const User = require('../models/user');
+
+const bcryptjs = require('bcryptjs')
+
 const router = express.Router();
 
 // SIGNUP
@@ -7,6 +10,25 @@ router.get('/signup', (req, res) => res.render('auth/signup'));
 
 router.post('/signup', (req, res, next) => {
   console.log('The form data: ', req.body);
+  const { firstName, lastName, email, password } = req.body;
+
+  bcryptjs
+   .genSalt(10)
+   .then(salt => bcryptjs.hash(password, salt))
+   .then(passwordHash => {
+     return User.create({
+       firstName,
+       lastName,
+       email,
+       hashedPassword: passwordHash
+     });
+   })
+   .then(userFromDB => {
+     console.log('Newly created user is: ', userFromDB);
+   })
+   .catch(error => next(error));
+
+
 });
 
 module.exports = router;
