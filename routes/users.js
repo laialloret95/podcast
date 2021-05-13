90dfcfd4a7f57a7b4f7d5f668004e3d28a7d86c4
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-05-13 17:09:46 +0200

    adds routes edit profile. WIP post route

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,36 @@ const checkIfUserIsLoggedIn = require('../middlewares/auth');
 const router = express.Router();
 
 //USER ROUTES JUST FOR THE APP MEMBERS - PRIVATE
-//router.use(checkIfUserIsLoggedIn);
+router.use(checkIfUserIsLoggedIn);
 
-// USER PROFILE
+// PROFILE
 router.get('/profile', (req, res, next) => {
     console.log(req.session.currentUser)
     res.render('users/profile', { userInSession: req.session.currentUser });
+});
+
+// EDIT PROFILE
+router.get('/profile/edit', (req, res, next) => {
+  res.render('users/edit-profile', { userInSession: req.session.currentUser });
+});
+
+router.post('/profile/edit', (req, res, next) => {
+  const { firstName, lastName, mail, preferences } = req.body;
+  const id  = req.session.currentUser._id;
+
+  User.findByIdAndUpdate( id, { firstName, lastName, mail, preferences }, { new: true })
+   .then(updatedUserDB => {
+     const { firstName, lastName, mail, preferences } = updatedUserDB;
+     req.session.currentUser = {
+      firstName,
+      lastName,
+      mail,
+      preferences
+    };
+   })
+   .then(() => res.redirect("/profile"))
+   .catch((error) => next(error));
+
 });
 
 
