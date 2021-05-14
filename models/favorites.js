b14bdc94ff2a8590704846b93393b12b289e4d8e
Laia Lloret <laialloret95@gmail.com>
2021-05-14 10:07:16 +0200

    feature of adding a favourite: user can add a fav podcast and when enetering in that podcast profile see that is included in her list

--- models/favorites.js
+++ models/favorites.js
@@ -2,8 +2,14 @@ const { Schema, model } = require('mongoose');
 
 const favoritesSchema = new Schema(
     {
-      podcastID: {type: [ Schema.Types.ObjectId ], ref: 'Podcast'},
-      usersID: {type: [ Schema.Types.ObjectId ], ref: 'User'}
+      podcastID: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'podcastID' 
+    },
+      userIDs: {
+        type: [ Schema.Types.ObjectId ], 
+        ref: 'User'
+    }
     },
     { timestamps: true }
 );
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Podcast = require('../models/podcast');
 const router = express.Router();
+const Favourite = require('../models/favorites');
 
 // HOME PAGE
 router.get('/', (req, res, next) => {
@@ -51,16 +52,26 @@ router.post('/podcasts/search', (req, res, next) => {
 router.get('/podcasts/profile/:id', (req, res, next) => {
     const { id } = req.params;
     let loggedUser;
-    console.log(req.session.currentUser);
+
     if (req.session.currentUser) {
+        // If user is logged
         loggedUser = true;
         Podcast
             .findById(id)
             .then(podcastDB => {
-            res.render('podcasts/profile', {podcastDB, loggedUser});
-            })
-            .catch(error => next(error));
+                Favourite.findOne({podcastID:id}, {userIDs: req.session.currentUser._id})
+                    .then(favouriteDB => {
+                        if (favouriteDB) {
+                        // If podcast has been favourited by the user
+                            res.render('podcasts/profile', {podcastDB, loggedUser, favouritedPocast: true});
+                        } else {
+                        // If podcast has not been favourited by the user
+                            res.render('podcasts/profile', {podcastDB, loggedUser});
+                        }
+                    });
+                });
     } else {
+        // If unlogged user
         Podcast
         .findById(id)
         .then(podcastDB => {
@@ -68,6 +79,37 @@ router.get('/podcasts/profile/:id', (req, res, next) => {
         })
         .catch(error => next(error));
     }
+});
+
+//FAVOURITES
+router.get('/favourites/:podID', (req, res, next) => {
+    const { podID } = req.params;
+    const userID = req.session.currentUser._id;
+
+    Favourite.findOne({podcastID: podID})
+        .then( podcastDB => {
+            if (!podcastDB) {
+                // If podcast has never been favorited by anu user
+                const newFavourite = new Favourite({podcastID: podID, userIDs: userID});
+                newFavourite
+                    .save()
+                    .then(() => {
+                        req.flash('success','Favourite Added!');
+                        res.redirect(`/podcasts/profile/${podID}`);
+                    });
+            }
+            else if (podcastDB.userIDs.includes(userID)) {
+                // If podcast has already been favorited by this user
+                req.flash('warning','You have already favourited this podcast!');
+                res.redirect(`/podcasts/profile/${podID}`);
+            }
+            else {
+                // If podcast has been favorited by another user
+                Favourite.findByIdAndUpdate(podID, { $push: { userIDs: userID } });
+                req.flash('success','Favourite Added!');
+                res.redirect(`/podcasts/profile/${podID}`);
+            }
+        });
 });
 
 module.exports = router;
