52cfe68f27bb8177d5b1f4692aaa3d28bff207bb
Laia Lloret <laialloret95@gmail.com>
2021-05-15 11:05:30 +0200

    config

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const hbs = require('hbs');
 hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
+const sassMiddleware = require('node-sass-middleware');
 
 // ℹ️ Connects to the database
 require('./configs/db');
@@ -36,6 +37,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use(flash());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true
+}));
 
 app.use((req, res, next) => {
   res.locals.flashMessage = req.flash('flashMessage');
